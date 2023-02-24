75113981e53e21a8f8303452cdee860facf50593
Bear Crump <ericbearcrump@gmail.com>
2023-02-23 18:45:42 -0600

    Timestamp 1:02:07

--- ShopOnline.Web/ShopOnline.Web/Program.cs
+++ ShopOnline.Web/ShopOnline.Web/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// the line below is DI  configuration for the HttpClient used by blazor FE. Uri argument was replaced with base URL in use by API
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7191/") });
 
 await builder.Build().RunAsync();
---
+++ ShopOnline.Web/ShopOnline.Web/Services/Contracts/IProductService.cs
@@ -0,0 +1,6 @@
+﻿namespace ShopOnline.Web.Services.Contracts
+{
+    public interface IProductService
+    {
+    }
+}
